8efd826d714e82992122e3d434481fc7328c532c
Cabelin.tz <wesleydias8007@gmail.com>
2026-09-10 20:26:43 -0300

    Update controller.php

--- sw/Atividade 0409/controller.php
+++ sw/Atividade 0409/controller.php
@@ -1,1 +1,43 @@
 
+<<<<<<< HEAD
+<?php
+
+require_once 'Model.php';
+
+$op1 = new Aluno();
+
+$op1->setNome($_POST['nome']);
+$op1->setNota1($_POST['nota1']);
+$op1->setNota2($_POST['nota2']);
+$op1->setNota3($_POST['nota3']);
+$op1->setFaltas($_POST['faltas']);
+
+echo "<h3>Resultado do Aluno: " . $op1->getNome() . "</h3>";
+echo "<p>Média: " . number_format($op1->calcularMedia(), 1) . "</p>";
+echo "<p>Presença: " . number_format($op1->calcularPresenca(), 1) . "%</p>";
+echo "<p>Situação: "; 
+$op1->mostrarResultado(); 
+echo "</p>";
+
+=======
+<?php
+
+require_once 'Model.php';
+
+$op1 = new Aluno();
+
+$op1->setNome($_POST['nome']);
+$op1->setNota1($_POST['nota1']);
+$op1->setNota2($_POST['nota2']);
+$op1->setNota3($_POST['nota3']);
+$op1->setFaltas($_POST['faltas']);
+
+echo "<h3>Resultado do Aluno: " . $op1->getNome() . "</h3>";
+echo "<p>Média: " . number_format($op1->calcularMedia(), 1) . "</p>";
+echo "<p>Presença: " . number_format($op1->calcularPresenca(), 1) . "%</p>";
+echo "<p>Situação: "; 
+$op1->mostrarResultado(); 
+echo "</p>";
+
+>>>>>>> 29178eb (ATIVIDADE DA JU SÁ)
+?>
